84afe9da8e9d6e3f7967521be530c8aad4758a31
juanccancelado <j.canceladod@uniandes.edu.co>
2025-03-17 16:32:14 -0500

    ajustes para poder probar ya que el import de array no funciona

--- DataStructures/Map/map_linear_probing.py
+++ DataStructures/Map/map_linear_probing.py
@@ -1,29 +1,43 @@
-from DataStructures.Map import map_functions as mf
-from DataStructures.Map import map_entry as me
-import random as rd
-from DataStructures.List import array_list as lt
+import map_functions as mf
+import map_entry as me
+#import array_list as lt
 
+def new_list():
+    new_list = {"elements": [],
+                "size": 0,
+                }
+    return new_list
+
+def add_last(my_list, element):
+    my_list["elements"].append(element)
+    my_list["size"] += 1
+    return my_list
+
+def get_element(my_list, index):
+    return my_list["elements"][index]
 
 def new_map(num_elements, load_factor, prime=109345121):
     
      capacity = int(mf.next_prime(num_elements/ load_factor))
 
-     x = lt.new_list()
+     x = new_list() #lt.
      dict = {"key": None, "value": None}
      for i in range(capacity):
-         lt.add_last(x, dict) 
+         add_last(x, dict) #lt.
 
-     map = {"prime": prime, "capacity": capacity, 'scale': rd.randint(1, prime - 1),
-           'shift': rd.randint(0, prime - 1),'table': x, "current_factor": 0, "limit_factor": load_factor,
+     map = {"prime": prime, "capacity": capacity, 'scale': 1,
+           'shift': 0,'table': x, "current_factor": 0, "limit_factor": load_factor,
              "size": 0} 
     
      return map
 
-#def put(my_map, key, value):
+def put(my_map, key, value):
+     
      hash = mf.hash_value(my_map["table"], key)
      slot = find_slot(my_map, key, hash)
 
      my_map["table"][slot] = 1
+
 
 def find_slot(my_map, key, hash_value):
    
@@ -35,21 +49,21 @@ def find_slot(my_map, key, hash_value):
       if is_available(my_map["table"], hash_value):
             if first_avail is None:
                first_avail = hash_value
-            entry = lt.get_element(my_map["table"], hash_value)
+            entry = get_element(my_map["table"], hash_value) #lt.
             if me.get_key(entry) is None:
                found = True
 
-      elif default_compare(key, lt.get_element(my_map["table"], hash_value)) == 0:
+      elif default_compare(key, get_element(my_map["table"], hash_value)) == 0: #lt.
             first_avail = hash_value
-            found = True
+            found = True 
             ocupied = True
       hash_value = (hash_value + 1) % my_map["capacity"]
    return ocupied, first_avail
 
 def is_available(table, pos):
 
-     entry = lt.get_element(table, pos)
-     if me.get_key(entry) is None or me.get_key(entry) == "__EMPTY__":
+     entry = get_element(table, pos)
+     if me.get_key(entry) is None or me.get_key(entry) == "__EMPTY__":#lt.
           result = True
      else:
          result = False
@@ -62,5 +76,9 @@ def default_compare(key, entry):
      elif key > me.get_key(entry):
           return 1
      return -1
+
+map = new_map(1,0.5)
+
+print(map)
 
 
